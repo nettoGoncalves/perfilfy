a0fba4be591e03b774c3ab81dec8f4a861149ea9
nettoGoncalves <jkneto.jcn@gmail.com>
2025-07-16 22:20:24 -0300

    finalizada hero section

--- app/components/landing-page/hero.tsx
+++ app/components/landing-page/hero.tsx
@@ -1,7 +1,13 @@
+import ProjectCard from "../commons/project-card";
+import { TotalVisits } from "../commons/total-visits";
+import UserCard from "../commons/user-card";
+import Button from "../ui/button";
+import TextInput from "../ui/text-input";
+
 export default function Hero() {
   return (
-    <div className="flex border">
-      <div className="w-full flex flex-col gap-2 mt-[35vh] border">
+    <div className="flex h-screen">
+      <div className="w-full flex flex-col gap-2 mt-[35vh]">
         <h1 className="text-5xl font-bold text-white leading-[64px]">
           Seus projeto e redes sociais em um unico link
         </h1>
@@ -10,24 +16,24 @@ export default function Hero() {
           <br />
           Acompanhe o engajamento com analytics de clicks
         </h2>
-        <div className="flex items-center gap-2 w-full -mt-[10vh]">
-          <span className="text-white text-xl">projectinbio.com/</span>
-          <input type="text" />
-          <button>Criar agora</button>
+        <div className="flex items-center gap-2 w-full mt-[10vh]">
+          <span className="text-white text-xl">projectinbio.com</span>
+          <TextInput placeholder="Seu Link" />
+          <Button>Criar agora</Button>
         </div>
       </div>
-      <div className="w-full flex items-center justify-center bg-[radial-gradient(circle_at_50%_50%,#4B2DBB, transparent_55%)]">
+      <div className="w-full flex items-center justify-center bg-[radial-gradient(circle_at_50%_50%,#4B2DBB,transparent_55%)]">
         <div className="relative">
-            {/* <UserCard /> */}
-            <div className="absolute -bottom-[7%] -right-[45%]">
-                {/* <TotalVisits /> */}
-            </div>
-            <div className="absolute top-[20%] -left-[45%] -z-10">
-                {/* <ProjectCard /> */}
-            </div>
-            <div className="absolute -top-[5%] -left-[55%] -z-10">
-                {/* <ProjectCard /> */}
-            </div>
+          <UserCard />
+          <div className="absolute -bottom-[7%] -right-[45%]">
+            <TotalVisits />
+          </div>
+          <div className="absolute top-[20%] -left-[45%] -z-10">
+            <ProjectCard />
+          </div>
+          <div className="absolute -top-[5%] -left-[55%] -z-10">
+            <ProjectCard />
+          </div>
         </div>
       </div>
     </div>
