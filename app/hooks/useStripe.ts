1ba96fe9a91cc1f3b7dcd1bdf7f58242dce2703c
nettoGoncalves <jkneto.jcn@gmail.com>
2025-07-30 00:02:17 -0300

    mais testes

--- app/hooks/useStripe.ts
+++ app/hooks/useStripe.ts
@@ -37,7 +37,17 @@ export function useStripe() {
         sessionId: data.sessionId,
       });
     } catch (error) {
-      console.error(error);
+      const response = await fetch("/api/stripe/create-checkout", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({ metadata, isSubscription }),
+      });
+
+      console.error("Erro ao criar checkout:", error);
+      const text = await response.text();
+      console.error("Resposta do servidor:", text);
     }
   }
 
